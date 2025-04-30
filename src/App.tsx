52c7b74a23f7a43fdc9dd2d66cb295d62c103b05
jmuslu <your_email@example.com>
2025-04-29 21:30:21 -0400

    changing app.tsx to hashrouter from browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Change this line:
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// To this:
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,17 +13,17 @@ import Apply from './pages/Apply';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="methodology" element={<Methodology />} />
-          <Route path="alliances" element={<Alliances />} />
-          <Route path="apply" element={<Apply />} />
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="methodology" element={<Methodology />} />
+            <Route path="alliances" element={<Alliances />} />
+            <Route path="apply" element={<Apply />} />
+          </Route>
+        </Routes>
+      </Router>
   );
 }
 
