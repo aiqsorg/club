2e82d28eaa57435441069372cde49b9c08028503
aiqsorg <aiqsorg@gmail.com>
2025-08-30 14:23:37 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // This disables dark mode completely
   theme: {
     extend: {
       fontFamily: {
